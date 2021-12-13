205dff5f937370c495d20f682b90ea79d622f3f2
Samuel Elias <samuel.elias.dev@gmail.com>
2021-12-13 08:11:21 -0300

    feat(menus/handler): add heritage handler in menu from core

---
+++ menus/src/main/java/samueldev/challenges/ifoodchallenge/menus/handler/MenuRestExceptionHandler.java
@@ -0,0 +1,8 @@
+package samueldev.challenges.ifoodchallenge.menus.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import samueldev.challenges.ifoodchallenge.core.handler.RestExceptionHandler;
+
+@ControllerAdvice
+public class MenuRestExceptionHandler extends RestExceptionHandler {
+}
